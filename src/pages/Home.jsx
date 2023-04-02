2231c5f374e020a0b9f922e8d9e0630428627e6b
AugustWahlberg <aug.wahlberg@gmail.com>
2023-04-02 18:11:46 +0200

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import * as S from "./Home.Styles";
 import { AiFillStar, AiFillEye } from "react-icons/ai";
-import { BsCartPlusFill } from "react-icons/bs";
 import { SearchBar } from "../components/SearchBar";
 import { Link } from "react-router-dom";
 import { usePosts } from "../api/getPosts";
